f2e1adc0a10e34c69a405cb1ee282786a6c3b0df
Paul Leverett <pleverett@gmail.com>
2024-07-17 14:43:22 -0700

    Finished lesson 6

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from "express";
 import createHomepageTemplate from "./views/index.js";
 import createListTemplate from "./views/list.js";
 import BOOKS_DATA from "./data/data.js";
+import createBookTemplate from "./views/book.js";
 
 // create app
 const app = express();
@@ -25,7 +26,14 @@ app.post("/books", (req, res) => {
 
   BOOKS_DATA.push({ id, title, author });
 
-  res.send(`<li>${title}, ${author}</li>`);
+  res.redirect(`/books/${id}`);
+});
+
+app.get("/books/:id", (req, res) => {
+  const { id } = req.params;
+  const book = BOOKS_DATA.find((b) => b.id === id);
+
+  res.send(createBookTemplate(book));
 });
 
 // listen to port
